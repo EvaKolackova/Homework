e7baca740f220e7e0f7e58301120f3f54e4c5482
EvaKolackova <73023962+EvaKolackova@users.noreply.github.com>
2020-11-15 12:53:07 +0100

    Update homework3.js

--- homework3.js
+++ homework3.js
@@ -2,10 +2,11 @@ describe('Secret number', () => {
   let hodnota, secretNumber, formular, overeni
   before(() => { //muzes zmenit na beforeEach, pokud je potreba
     browser.url('https://automation.cervik.repl.co/inputs.html')
-    
+ 
    })
 
   it('confirmation appears after validating correct secret number', () => {
+    // DOPLN
     secretNumber = browser.$('[name="secret"]')
     hodnota = (secretNumber.getValue())
     formular =  browser.$('input#secretNumberInput').addValue(hodnota)
@@ -15,6 +16,7 @@ describe('Secret number', () => {
   })
 
   it('error appears after validating wrong secret number', () => {
+    browser.refresh()
     formular =  browser.$('input#secretNumberInput').addValue("blabla")
     browser.$("button#checkSecretNumber").click()
     overeni = browser.$('small#passwordHelpBlock')
@@ -23,9 +25,10 @@ describe('Secret number', () => {
 
 
   it('error appears after validating empty secret number', () => {
+    browser.refresh()
+    formular =  browser.$('input#secretNumberInput').addValue("")
     browser.$("button#checkSecretNumber").click()
     overeni = browser.$('small#passwordHelpBlock')
     expect(overeni).toHaveText("Super secret je super špatně.")
   })
 })
-                
